4dee8b82e44fecc123761f634ef779a79cc45091
Meera <meera7419@gmail.com>
2020-05-24 17:20:38 -0400

    Face detection in real time using two different algorithm simultaneously

--- Face_detection_realtime_Haar_LBP.py
+++ Face_detection_realtime_Haar_LBP.py
@@ -14,13 +14,14 @@ def main():
 
     while True:
         # Capture frame-by-frame
+        start = time.time()
         ret, frame = video_capture.read()
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces1,p,confidence1 = classifier1.detectMultiScale3(gray,scaleFactor=1.05,minNeighbors= 8,outputRejectLevels = True)
 
-        # Draw a Red rectangle around the faces identified by HAAR_Cascade
+        # Draw a Green rectangle around the faces identified by HAAR_Cascade
         for (x, y, w, h),conf1 in zip(faces1,confidence1):
             if conf1>1:
                 rectangle(frame, (x, y), (x+w, y+h), (0,255,0),2)
@@ -34,19 +35,18 @@ def main():
             if conf2>1:
                 rectangle(frame, (x, y), (x + w, y + h),(255,0,0), 2)
 
-        # Display the resulting frame
         cv2.imshow('Video', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        end = time.time()
+        print('time taken to execute one frame ', end - start)
 
-    # When everything is done, release the capture
     video_capture.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
 
-    start = time.time()
+
     main()
-    end = time.time()
-    print('time taken to execute: ',end - start)
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CV_term_project
+
+source file contains three face detection codes.
+1.Face_detection_Haar_C.py
+2.Face_detection_LBP.py
+3.Face_detection_MTCNN.py
+
+One combined approach of face detection is implemented in following file.
+4. Face_detection_realtime_Haar_LBP.py
+
+First three python files uses images from "Dataset" directory as input images.
+Outputs are stored in the output directory.
+Face_detection_Haar_C.py and Face_detection_LBP.py uses pretrained models, so their xml files are also included in the source file. 
+
+Face_detection_realtime_Haar_LBP.py needs to access the device camera. Please grant the permission for it when asked.
